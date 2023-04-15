7407d14bbd5b14cb0f3d6d2e0472d14d39f2b4eb
Vadim Cherepanov <xenos26@gmail.com>
2023-04-15 11:41:52 +0700

    feat: add dispatch

--- ReduxProject/src/App.jsx
+++ ReduxProject/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
 
 function App() {
+  const dispatch = useDispatch()
   return (
     <div className='button__wrapper' >
       <button className='button'>Add cash</button>
