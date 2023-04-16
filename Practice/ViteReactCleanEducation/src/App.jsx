141f429d3ce351f8385e90ba266f53b8ca5e30be
Vadim Cherepanov <xenos26@gmail.com>
2023-04-16 16:31:22 +0700

    fix: delete border from cash count

--- Practice/ViteReactCleanEducation/src/App.jsx
+++ Practice/ViteReactCleanEducation/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div className='grid justify-center content-center items-center grid-cols-[200px_200px] grid-rows-[100px_100px] gap-2 h-full w-full fixed'>
-      <div className='col-span-2 text-3xl m-auto border-2 border-solid border-black'>{cash}</div>
+      <div className='col-span-2 text-3xl m-auto'>{cash}</div>
       <button className='w-48 h-20 bg-cyan-500 border-2 border-solid text-white rounded-lg' onClick={() => addCash(Number(prompt()))}>Add cash</button>
       <button className='w-48 h-20 bg-cyan-500 border-2 border-solid text-white rounded-lg' onClick={() => getCash(Number(prompt()))}>Get cash</button>
     </div>
