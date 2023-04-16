4c7c7d2e08119042adc4d150ae91871f0cdfb133
Vadim Cherepanov <xenos26@gmail.com>
2023-04-16 19:30:39 +0700

    chore: decomposite store and reducers

---
+++ Practice/ViteReactCleanEducation/src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore, combineReducers } from "redux"
+import cashReducer from "./cashReducer"
+import customerReducer from "./customerReducer"
+
+const rootReducer = combineReducers({
+  cash: cashReducer,
+  customers: customerReducer,
+})
+
+const store = createStore(rootReducer)
+
+export default store
