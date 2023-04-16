a4d45333ff99998f4fd7185ae072252d931cad4d
Vadim Cherepanov <xenos26@gmail.com>
2023-04-16 16:42:33 +0700

    chore: add all redux code again with prettier

--- Practice/ViteReactCleanEducation/src/main.jsx
+++ Practice/ViteReactCleanEducation/src/main.jsx
@@ -4,10 +4,26 @@ import App from './App'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
+const initialState = {
+  cash: 5,
+}
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_CASH":
+      return { ...state, cash: state.cash + action.payload }
+    case "GET_CASH":
+      return { ...state, cash: state.cash - action.payload }
 
+
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider >
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
