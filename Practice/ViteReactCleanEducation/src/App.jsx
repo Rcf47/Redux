84111664006099b8efe55835609a742e698a21ef
Vadim Cherepanov <xenos26@gmail.com>
2023-04-16 16:32:36 +0700

    chore: delete all code for redux repeat

--- Practice/ViteReactCleanEducation/src/App.jsx
+++ Practice/ViteReactCleanEducation/src/App.jsx
@@ -2,16 +2,6 @@ import { useDispatch, useSelector } from 'react-redux'
 
 
 function App() {
-  const dispatch = useDispatch()
-  const cash = useSelector(state => state.cash)
-
-  const addCash = (cash) => {
-    dispatch({ type: "ADD_CASH", payload: cash })
-  }
-
-  const getCash = (cash) => {
-    dispatch({ type: "GET_CASH", payload: cash })
-  }
 
   return (
     <div className='grid justify-center content-center items-center grid-cols-[200px_200px] grid-rows-[100px_100px] gap-2 h-full w-full fixed'>
--- Practice/ViteReactCleanEducation/src/main.jsx
+++ Practice/ViteReactCleanEducation/src/main.jsx
@@ -4,27 +4,10 @@ import App from './App'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
-const defaultState = {
-  cash: 5,
-}
-
-const reducer = (state = defaultState, action) => {
-  switch (action.type) {
-    case "ADD_CASH":
-      return { ...state, cash: state.cash + action.payload }
-    case "GET_CASH":
-      return { ...state, cash: state.cash - action.payload }
-
-    default:
-      return state;
-  }
-}
-
-const store = createStore(reducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider >
       <App />
     </Provider>
   </React.StrictMode>,
